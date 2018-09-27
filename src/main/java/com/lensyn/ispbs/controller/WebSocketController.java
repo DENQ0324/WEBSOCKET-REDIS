e110229b097a7720baa77c513c7fcbaf8f96d0ce
邓倩 <dengq@lensyn.com>
2018-09-27 16:16:47 +0800

    Signed-off-by: 邓倩 <dengq@lensyn.com>

--- src/main/java/com/lensyn/ispbs/controller/WebSocketController.java
+++
@@ -1,9 +0,0 @@
-package com.lensyn.ispbs.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestParam;
-
